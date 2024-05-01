228f60380a26eb959a7a0d549a10a0c9c468cc47
lanlanlanlanlemonsoda <651639688@qq.com>
2024-05-01 21:54:46 +0800

    update profileScreen

--- FIT5046_A1/app/src/main/java/com/example/a1/ProfileScreen.kt
+++ FIT5046_A1/app/src/main/java/com/example/a1/ProfileScreen.kt
@@ -1,259 +1,269 @@
 package com.example.a1
 
+import androidx.compose.runtime.Composable
 
-import androidx.compose.material3.*
-import android.app.DatePickerDialog
+import androidx.navigation.compose.rememberNavController
+//noinspection UsingMaterialAndMaterial3Libraries
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import java.text.SimpleDateFormat
-import java.util.*
+import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+import androidx.navigation.compose.rememberNavController
+import com.example.a1.ui.theme.A1Theme
+
 
 @Composable
 fun ProfileScreen(navController: NavHostController) {
-    // Prepare states for date picker and dropdowns
     val context = LocalContext.current
-    var dateOfBirth by remember { mutableStateOf("11 Jan 2000") }
-    val calendar = Calendar.getInstance()
-    val dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
-    val validHeightRange = 50..300
-    val validWeightRange = 10..300
-    val heightErrorState = remember { mutableStateOf(false) }
-    val weightErrorState = remember { mutableStateOf(false) }
+    var name by remember { mutableStateOf("") }
+    var dateOfBirth by remember { mutableStateOf("") }
+    var sex by remember { mutableStateOf("") }
+    var height by remember { mutableStateOf("") }
+    var weight by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    var nameError by remember { mutableStateOf(false) }
+    var heightError by remember { mutableStateOf(false) }
+    var weightError by remember { mutableStateOf(false) }
 
-    // States for height and weight
-    var heightInput by remember { mutableStateOf("170") }
-    var weightInput by remember { mutableStateOf("65") }
+    val sexOptions = listOf("Female", "Male", "Other", "Prefer not to say")
 
+    val auth = FirebaseAuth.getInstance()
+    val user = auth.currentUser
+    val uid = user?.uid
+    val db = FirebaseFirestore.getInstance()
 
-
-    // Show DatePickerDialog
-    val showDatePickerDialog: () -> Unit = {
-
-        DatePickerDialog(
-            context,
-            { _, year, month, dayOfMonth ->
-                calendar.set(year, month, dayOfMonth)
-                dateOfBirth = dateFormat.format(calendar.time)
-            },
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH)
-        ).show()
+    LaunchedEffect(key1 = uid) {
+        if (uid != null) {
+            db.collection("usersInfo").document(uid).get().addOnSuccessListener { document ->
+                if (document.exists()) {
+                    name = document.getString("name") ?: ""
+                    dateOfBirth = document.getString("dateOfBirth") ?: ""
+                    sex = document.getString("sex") ?: ""
+                    height = document.getString("height") ?: ""
+                    weight = document.getString("weight") ?: ""
+                }
+            }.addOnFailureListener { e ->
+                Log.w("Firestorm", "Error fetching document", e)
+            }
+        }
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        Color(0xFFE4E4FC), // Top half background color
-                        Color(0xFFFAE8E1)  // Bottom half background color
-                    )
-                )
-            )
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Brush.verticalGradient(colors = listOf(Color(0xFFE4E4FC), Color(0xFFFAE8E1))))
     ) {
-        Scaffold(containerColor = Color.Transparent) { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top // 从顶部开始排列
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
 
-                Spacer(modifier = Modifier.height(40.dp))
-                Text(
-                    text = "Bella Z",
-                    color = Color(0xFF151C57),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 45.sp
-                )
-                Spacer(modifier = Modifier.height(40.dp))
-                Text(
-                    text = "Detail",
-                    color = Color(0xFF8E91B9),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp
-                )
-                Text(
-                    text = "This information ensures Fitness and Health data are accurate as possible",
-                    color = Color(0xFF8E91B9),
-                    fontSize = 14.sp
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                DetailItemWithDropdown(
-                    label = "     Date of Birth",
-                    value = dateOfBirth,
-                    onValueChange = { dateOfBirth = it },
-                    onClick = { showDatePickerDialog() }
-                )
-                InputFieldItem(
-                    label = "Height (cm)",
-                    value = heightInput,
-                    range = validHeightRange,
-                    onValueChange = { heightInput = it },
-                    errorState = heightErrorState
-                )
+            Text(
+                "Fitness and Health Details",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color(0xFF151C57),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
 
-                // Weight input field with validation
-                InputFieldItem(
-                    label = "Weight (kg)",
-                    value = weightInput,
-                    range = validWeightRange,
-                    onValueChange = { weightInput = it },
-                    errorState = weightErrorState
-                )
+            // Subtitle
+            Text(
+                "This information ensures Fitness and Health data are as accurate as possible",
+                fontSize = 16.sp,
+                color = Color(0xFF8E91B9),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+        }
 
-                //logout button
+        Spacer(modifier = Modifier.height(8.dp))
+
+    Card(
+            shape = RoundedCornerShape(14.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.BottomCenter),
+            elevation = 2.dp
+        ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            InformationRow("Name", name, onValueChange = {
+                name = it
+                nameError = it.any { char -> !char.isLetter() } || it.length > 20
+            }, error = nameError)
                 Spacer(modifier = Modifier.height(16.dp))
+
+                InformationRow(
+                    label = "Date of Birth",
+                    value = dateOfBirth,
+                    readOnly = true,
+                    onClick = {
+                        showDatePicker(context) { newDate -> dateOfBirth = newDate }
+                    })
+                Spacer(modifier = Modifier.height(16.dp))
+
+                InformationRow(label = "Sex", value = sex, readOnly = true, onClick = {
+                    expanded = true
+                })
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    sexOptions.forEach { option ->
+                        DropdownMenuItem(onClick = {
+                            sex = option
+                            expanded = false
+                        }) {
+                            Text(option)
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+            InformationRow("Height (cm)", height, onValueChange = {
+                height = it
+                heightError = it.toIntOrNull()?.let { num -> num < 1 || num > 400 } ?: true
+            }, error = heightError)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+            InformationRow("Weight (kg)", weight, onValueChange = {
+                weight = it
+                weightError = it.toIntOrNull()?.let { num -> num < 1 || num > 400 } ?: true
+            }, error = weightError)
+
                 Button(
                     onClick = {
-                        // Firebase Auth instance
-                        val auth = FirebaseAuth.getInstance()
+                        saveUserInfo(
+                            db,
+                            uid,
+                            name,
+                            dateOfBirth,
+                            sex,
+                            height,
+                            weight,
+                            context,
+                            navController
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF776EE3)),
+                ) {
+                    Text("Save Changes", color = Color.White, fontSize = 16.sp)
 
-                        // Sign out the current user
+                }
+
+                Button(
+                    onClick = {
+                        // Handle logout logic
                         auth.signOut()
-
-                        // Navigate to the welcome screen
-                        navController.navigate("welcome") {
-                            popUpTo("welcome") { inclusive = true }
+                        navController.navigate("login") {
+                            popUpTo("profile") { inclusive = true } // Clear back stack up to profile screen
                         }
                     },
                     modifier = Modifier
-                        .height(50.dp)
-                        .width(180.dp),
-                    shape = RoundedCornerShape(25.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF776EE3))
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF776EE3))
                 ) {
-                    Text("Logout", color = Color.White, fontSize = 16.sp)
+                    Text("Log out", color = Color.White, fontSize = 16.sp)
                 }
+
+
+
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailItemWithDropdown(
+fun InformationRow(
     label: String,
     value: String,
-    options: List<String> = emptyList(),
-    expanded: Boolean = false,
     onValueChange: (String) -> Unit = {},
-    onExpandedChange: (Boolean) -> Unit = {},
+    readOnly: Boolean = false,
+    error: Boolean = false,
     onClick: (() -> Unit)? = null
 ) {
-    Column {
-        Text(
-            text = label,
-            style = MaterialTheme.typography.labelLarge,
-            color = MaterialTheme.colorScheme.onSurfaceVariant
-        )
-        if (options.isNotEmpty()) {
-            // It's a dropdown menu for height or weight
-            var selectedIndex by remember { mutableIntStateOf(options.indexOf(value)) }
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = onExpandedChange
-            ) {
-                OutlinedTextField(
-                    value = value,
-                    onValueChange = { },
-                    readOnly = true,
-                    label = { Text(label) },
-                    trailingIcon = {
-                        Icon(
-                            imageVector = if (expanded) Icons.Filled.ArrowDropUp else Icons.Filled.ArrowDropDown,
-                            contentDescription = if (expanded) "Close menu" else "Open menu"
-                        )
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { onExpandedChange(false) }
-                ) {
-                    options.forEachIndexed { index, option ->
-                        DropdownMenuItem(
-                            text = { Text(option) },
-                            onClick = {
-                                onValueChange(option)
-                                selectedIndex = index
-                                onExpandedChange(false)
-                            }
-                        )
-                    }
+    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Text(label, modifier = Modifier.weight(1f))
+
+        if (readOnly) {
+            if (onClick != null) {
+                Button(onClick = onClick) {
+                    Text(value)
                 }
+            } else {
+                Text(value, modifier = Modifier.weight(2f))
             }
-        } else if (onClick != null) {
-            // It's the date picker for date of birth
-            OutlinedButton(onClick = onClick) {
-                Text(value)
-            }
-        }
-    }
-}
-
-
-
-
-
-
-@Composable
-fun InputFieldItem(
-    label: String,
-    value: String,
-    range: IntRange,
-    onValueChange: (String) -> Unit,
-    errorState: MutableState<Boolean>
-) {
-    val isError = errorState.value
-    val errorMessage = "Enter a number between ${range.first} and ${range.last}"
-
-    Column {
-        OutlinedTextField(
-            value = value,
-            onValueChange = { newValue ->
-                // 更新状态值，无论它是否在范围内
-                onValueChange(newValue)
-                // 更新错误状态
-                errorState.value = newValue.toIntOrNull()?.let { it !in range } ?: true
-            },
-            label = { Text(label) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            isError = isError
-        )
-        // 显示错误消息（如果有）
-        if (isError && value.isNotEmpty()) { // 添加了 value.isNotEmpty() 来防止在输入为空时显示错误消息
-            Text(
-                text = errorMessage,
-                color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.bodySmall, // 这里使用了 MaterialTheme.typography.bodySmall 替代了 TextStyle
-                modifier = Modifier.padding(start = 16.dp, top = 4.dp)
+        } else {
+            TextField(
+                value = value,
+                onValueChange = onValueChange,
+                isError = error,
+                singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    errorIndicatorColor = if (error) Color.Red else Color.Transparent
+                )
             )
+            if (error) {
+                Text("Invalid input", color = Color.Red, style = MaterialTheme.typography.caption)
+            }
         }
     }
 }
+
+
+
+
+fun saveUserInfo(db: FirebaseFirestore, uid: String?, name: String, dateOfBirth: String, sex: String, height: String, weight: String, context: Context, navController: NavHostController) {
+    if (uid != null) {
+        val userInfo = hashMapOf(
+            "name" to name,
+            "dateOfBirth" to dateOfBirth,
+            "sex" to sex,
+            "height" to height,
+            "weight" to weight
+        )
+        db.collection("usersInfo").document(uid).set(userInfo)
+            .addOnSuccessListener {
+                Toast.makeText(context, "Information saved successfully", Toast.LENGTH_SHORT).show()
+                navController.navigate("home") { popUpTo("home") { inclusive = true } }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(context, "Failed to save information: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+    }
+}
+
+
